20e6f6565a9f1f5d3850b9ba2c49427483e5f2da
geekvivek01 <31138071+geekvivek01@users.noreply.github.com>
2022-05-16 22:19:56 +0530

    Add files via upload

---
+++ Requester/Submitrequestsuccess.php
@@ -0,0 +1,57 @@
+<?php 
+define('TITLE', 'Success');
+define('PAGE', 'ServiceRequest');
+include('includes/header.php');
+include('../dbconnection.php');
+session_start();
+if($_SESSION['is_login']){
+ $rEmail = $_SESSION['rEmail'];
+} else {
+ echo "<script> location.href='Requesterlogin.php'</script>";
+}
+
+$sql = "SELECT * FROM submitrequest_tb WHERE request_id ={$_SESSION['myid']}";
+$result = $conn->query($sql);
+if($result->num_rows == 1){
+$row = $result->fetch_assoc();
+ $product_info = $row['request_info'];
+  $product = explode(",", $product_info);
+ echo "<div class='ml-5 mt-5'>
+ <table class='table'>
+  <tbody>
+   <tr>
+     <th>Request ID</th>
+     <td>".$row['request_id']."</td>
+   </tr>
+   <tr>
+     <th>Name</th>
+     <td>".$row['requester_name']."</td>
+   </tr>
+   <tr>
+   <th>Email ID</th>
+   <td>".$row['requester_email']."</td>
+  </tr>
+   <tr>
+    <th>Brand</th>
+    <td>".$product[0]."</td>
+   </tr>
+   <tr>
+    <th>Product</th>
+    <td>".$product[1]."</td>
+   </tr>
+   <tr>
+    <th>Request Description</th>
+    <td>".$row['request_desc']."</td>
+   </tr>
+
+   <tr>
+    <td><form class='d-print-none'><input class='btn btn-danger' type='submit' value='Print' onClick='window.print()'></form></td>
+  </tr>
+  </tbody>
+ </table> </div>
+ ";
+} else {
+ echo "Failed";
+}
+include('includes/footer.php');
+?>
